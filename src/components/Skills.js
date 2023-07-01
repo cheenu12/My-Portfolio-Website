29f32d1be488ad122a44ca6a85ec79f2e8e68854
Chirag Batheja <99245027+cheenu12@users.noreply.github.com>
2023-07-01 10:06:46 +0530

    Add files via upload

---
+++ src/components/Skills.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Carousel from "react-multi-carousel";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import "react-multi-carousel/lib/styles.css";
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>
+                MERN stack development, DSA, Problem-Solving, C++ and effective
+                leadership.
+                <br></br> Passionate about lifelong learning in technologies.
+              </p>
+
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="owl-carousel owl-theme skill-slider "
+                customRightArrow={<img src={arrow1} alt="right-arrow" />}
+                customLeftArrow={<img src={arrow2} alt="left-arrow" />}
+              >
+                <div className="item">
+                  <img src={meter1} alt="pro" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="pro" />
+                  <h5>Data Structures & Algorithms</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="pro" />
+                  <h5>Leadership</h5>
+                </div>
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="pro" />
+    </section>
+  );
+};
